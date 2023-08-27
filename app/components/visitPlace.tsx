3f2df7a71c5df1144b342cbaf979fe79eee8f441
Mateusz Wszola <wszola.m@gmail.com>
2023-08-27 14:52:08 +0200

    fix: optimistically update the place visited flag

--- app/components/visitPlace.tsx
+++ app/components/visitPlace.tsx
@@ -1,22 +1,30 @@
 import { useFetcher } from "@remix-run/react";
 
-export function VisitPlace({ placeId, isVisited }: { placeId: string; isVisited: boolean }) {
+export function VisitPlace({
+  placeId,
+  isVisited,
+}: {
+  placeId: string;
+  isVisited: boolean;
+}) {
   const visitPlace = useFetcher();
+
+  let visited = isVisited;
+
+  if (visitPlace.formData) {
+    visited = visitPlace.formData.get('visited') === 'yes';
+  }
 
   return (
     <visitPlace.Form method="POST">
       <input type="hidden" name="placeId" value={placeId} />
-      <input
-        type="hidden"
-        name="visited"
-        value={isVisited ? "no" : "yes"}
-      />
+      <input type="hidden" name="visited" value={visited ? "no" : "yes"} />
       <button
         onClick={(event) => {
           event.stopPropagation();
         }}
       >
-        {isVisited ? (
+        {visited ? (
           <span aria-label="Remove visited status">📌</span>
         ) : (
           <span className="opacity-25" aria-label="Mark as visited">
